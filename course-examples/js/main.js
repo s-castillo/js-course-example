ffecd96e3000f61a7535f7a5c4bebd93d0c9ce04
s.castillo <s.castillo.graphics@gmail.com>
2018-03-11 14:56:04 -0500

    Adding proper link for .js file and removing my incorrect js from main

--- course-examples/js/main.js
+++ course-examples/js/main.js
@@ -1,2 +1,40 @@
 // # START EDITING YOUR JAVASCRIPT HERE
 // ===============
+
+// Making a simple variable for the calculator 
+const calculator = document.querySelector('.calculator')
+
+// Making a simple variable for the keys - we're looking inside the variable calculator which we've established is the parent div of .calculator which has .calculator__keys children.
+const keys = calculator.querySelector('.calculator__keys')
+
+
+// Adding event listener to our keys
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        // Do something
+    }
+})
+
+const key = e.target
+const action = key.dataset.action
+
+if (
+    action === 'add' ||
+    action === 'subtract' ||
+    action === 'multiply' ||
+    action === 'divide'
+) {
+    console.log('operator key!')
+}
+
+if (action === 'decimal') {
+    console.log('decimal key!')
+}
+
+if (action === 'clear') {
+    console.log('clear key!')
+}
+
+if (action === 'calculate') {
+    console.log('equal key!')
+}
